6332b65c43a141f9e5b7db1806e485e0c0957118
Paulo Victor <paulpbi16@gmail.com>
2024-02-14 09:29:08 -0300

    classe 'Personalizacao' criada para colocar todos os efeitos visuais do projeto

---
+++ src/util/Personalizacao.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Personalizacao {
+    public static final String COR_RESET = "\u001b[0m";
+    public static final String COR_VERDE = "\u001b[32m";
+    public static final String COR_AZUL = "\u001b[34m";
+    public static final String COR_ROXA = "\u001b[35m";
+    public static final String COR_VERMELHA = "\u001b[31m";
+
+    public static void logoAscii() {
+        System.out.println(COR_AZUL + " __  __          _ _____            _     _            ");
+        System.out.println("|  \\/  |        | |  __ \\          (_)   | |           ");
+        System.out.println("| \\  / | ___  __| | |__) |___  __ _ _ ___| |_ ___ _ __ ");
+        System.out.println("| |\\/| |/ _ \\/ _` |  _  // _ \\/ _` | / __| __/ _ \\ '__|");
+        System.out.println("| |  | |  __/ (_| | | \\ \\  __/ (_| | \\__ \\ ||  __/ |   ");
+        System.out.println("|_|  |_|\\___|\\__,_|_|  \\_\\___|\\__, |_|___/\\__\\___|_|   ");
+        System.out.println("                               __/ |                   ");
+        System.out.println("                              |___/                    ");
+        System.out.println(COR_RESET + "\nRepositorio: https://github.com/paulopbi/medregister");
+    }
+}
