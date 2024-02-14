c5d260db7a85857c4752cceca8eff81eceeeff5e
Paulo Victor <paulpbi16@gmail.com>
2024-02-14 09:26:19 -0300

    criacao do construtor, encapsulamento e metodo alterar dados da classe Paciente

--- src/entities/Paciente.java
+++ src/entities/Paciente.java
@@ -1,4 +1,14 @@
 package entities;
 
-public class Paciente {
+public class Paciente extends Pessoa{
+    private boolean isDoadorDeOrgaos;
+    private boolean isAlergico;
+    private String preDiagnostico;
+    private byte nivelDeUrgencia;
+
+    @Override
+    public void alterarDados() {
+
+    }
+
 }
