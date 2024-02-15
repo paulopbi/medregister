cd99b35315b7b8208b86892c58c4a7bc7ce4f430
Paulo Victor <paulpbi16@gmail.com>
2024-02-14 21:07:59 -0300

    feat: menu para o cadastro de funcionarios, alteracao das cores e exibicao dos dados

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -1,6 +1,5 @@
 package entities;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -18,15 +17,10 @@ public class Funcionario extends Pessoa {
     public Funcionario() {
     }
 
-    public Funcionario(String nome, String cpf, String rg, String dataDeNascimento, char sexo, String telefone, String profissao, String nivelEscoalaridade, LocalDate dataAdmissao) {
-        super(nome, cpf, rg, dataDeNascimento,sexo,telefone);
-        setProfissao(profissao);
-        setNivelDeEscolaridade(nivelEscoalaridade);
-        setDataAdmissao(dataAdmissao);
-    }
-
-    public void setDataAdmissao(LocalDate dataAdmissao) {
-        this.dataAdmissao = dataAdmissao;
+    public Funcionario(String nome, String cpf, String rg, String dataDeNascimento, char sexo, String telefone, String profissao, String nivelEscoalaridade) {
+        super(nome, cpf, rg, dataDeNascimento, sexo, telefone);
+        this.setProfissao(profissao);
+        this.setNivelDeEscolaridade(nivelEscoalaridade);
     }
 
     public String getProfissao() {
@@ -45,14 +39,58 @@ public class Funcionario extends Pessoa {
         this.nivelDeEscolaridade = nivelDeEscolaridade;
     }
 
-    public String formatDataAdmissao(){
+    public String formatDataAdmissao() {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return dataAdmissao.format(fmt);
+    }
+
+    public void cadastrarFuncionario() {
+        System.out.println("\n---------- Cadastro De Funcionarios ----------");
+        sc = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome: ");
+        String nome = sc.nextLine();
+        super.setNome(nome);
+
+        System.out.print("Digite o seu CPF: ");
+        String cpf = sc.nextLine();
+        super.setCpf(cpf);
+
+        System.out.print("Digite o seu RG: ");
+        String rg = sc.nextLine();
+        super.setRg(rg);
+
+        System.out.print("Digite a sua data de nascimento: ");
+        String nascimento = sc.nextLine();
+        super.setDataDeNascimento(nascimento);
+
+        System.out.print("Digite o sexo (M ou F): ");
+        char sexo = sc.next().charAt(0);
+        super.setSexo(sexo);
+
+        System.out.print("Digite o seu telefone: ");
+        sc.nextLine();
+        String telefone = sc.nextLine();
+        super.setTelefone(telefone);
+
+        System.out.print("Digite a sua profissao: ");
+        String profissao = sc.nextLine();
+        this.setProfissao(profissao);
+
+        System.out.print("Digite o seu nivel de escolaridade: ");
+        String escolaridade = sc.nextLine();
+        this.setNivelDeEscolaridade(escolaridade);
+
+        funcionario = new Funcionario(nome, cpf, rg, nascimento, sexo, telefone, profissao, escolaridade);
+
+        System.out.println(COR_VERDE + "\n[+] Dados Cadastrados" + COR_RESET);
+        System.out.println(funcionario);
     }
 
     @Override
     public void alterarDados() {
         sc = new Scanner(System.in);
+        System.out.println(COR_AMARELA + "\n[ATENCAO] Alteracao de dados" + COR_RESET);
         System.out.print("Digite o seu nome: ");
         String nome = sc.nextLine();
         setNome(nome);
@@ -82,39 +120,11 @@ public class Funcionario extends Pessoa {
         String profissao = sc.nextLine();
         setProfissao(profissao);
 
-        System.out.print("Digite sua data de admissão (DD/MM/YYYY): ");
-        String dataInput = sc.next();
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // Formatação da data
-
-        // Gerenciamento de excessões com try-catch
-        try {
-            // Converte a string para um objeto LocalDate
-            LocalDate dataAdmissao = LocalDate.parse(dataInput, fmt);
-
-            // Exibe a data formatada
-            System.out.println("Data de admissão: " + dataAdmissao.format(fmt));
-            setDataAdmissao(dataAdmissao);
-
-            Duration contratoTime = Duration.between(dataAdmissao.atStartOfDay(), LocalDate.now().atStartOfDay());
-            System.out.println("O contrato vigora há: " + contratoTime.toDays() + " dias");
-        } catch (java.time.format.DateTimeParseException e) {
-            System.out.println("Formato de data inválido. Use o formato dd/MM/yyyy.");
-        }
-
-        System.out.print("Digite o seu nível de escolaridade: " + COR_RESET);
+        System.out.print("Digite o seu nivel de escolaridade: ");
         String escolaridade = sc.nextLine();
         setNivelDeEscolaridade(escolaridade);
 
-        System.out.println(COR_VERDE + "\n----Dados Atualizados----");
-        System.out.println(super.toString() + "\nProfissao: " + this.getProfissao()
-        + "\nNivel de escolaridade: " + this.getNivelDeEscolaridade() + COR_RESET);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString()
-                + "\nProfissao: " + this.getProfissao()
-                + "\nData de admissão: " + formatDataAdmissao()
-                + "\nEscolaridade: " + this.getNivelDeEscolaridade();
+        System.out.println(COR_VERDE + "\n[+] Dados Atualizados" + COR_RESET);
+        System.out.println(super.toString() + "\nProfissao: " + this.getProfissao() + "\nEscolaridade: " + this.getNivelDeEscolaridade());
     }
 }
